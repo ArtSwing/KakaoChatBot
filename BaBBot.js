a887f58aef7d7e43cfd8df64b62134b9beb791ab
ArtSwing <hbyoo94@naver.com>
2021-04-13 23:14:00 +0900

    !채팅기록 추가 유한빈
    !채팅기록 사용가능

--- BaBBot.js
+++ BaBBot.js
@@ -2,6 +2,11 @@ const FS = FileStream, path = '/sdcard/chat.json';
 if (!FS.read(path)) FS.write(path, '[]');
 var chat = JSON.parse(FS.read(path));
 
+chatpath = "sdcard/chatreport/chats"
+all = "All_Room_Chats"
+L = "-".repeat(40)
+Lw = "\u200b".repeat(500)
+
 
 /*////////////////////////////////////
   전역변수 설정 부분입니다.
@@ -685,6 +690,178 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
     }
 
     FS.write(path, JSON.stringify(chat));
+
+    try {
+
+if (!FS.read(chatpath)) FS.write(chatpath, "{}");
+
+if (msg) {
+
+json = JSON.parse(FS.read(chatpath))
+
+json[room] == undefined&&(json[room] = []);
+
+json[all] == undefined&&(json[all] = []);
+
+msg = msg.replace(/\u202E/g, "")
+
+sender = sender.replace(/\u202E/g, "")
+
+json[room].push(sender+" : "+msg+"\n\n("+new Date().toLocaleString().split("일 ")[1].split(" G")[0]+")")
+
+json[all].push("방 : "+room+" ("+(room==sender?"개인챗":"그룹챗")+")\n\n"+sender+" : "+msg+"\n\n("+new Date().toLocaleString().split("일 ")[1].split(" G")[0]+")")
+
+FS.write(chatpath, JSON.stringify(json))
+
+}
+
+//채팅 보기 (사람)
+
+if (msg.startsWith("!채팅보기")) {
+
+send = msg.substr(5).trim()
+
+if ((JSON.parse(FS.read(chatpath))[all].filter(a=>a.split("\n\n")[1].split(" : ")[0].trim() == send)).length!=0) {
+
+sendchat = JSON.parse(FS.read(chatpath))[all].filter(a=>a.split("\n\n")[1].split(" : ")[0].trim() == send)
+
+if (!back) {
+
+rep = L+"\n\n"+sendchat.join("\n\n"+L+"\n\n")+"\n\n"+L
+
+} else {
+
+rep = L+"\n\n"+sendchat.reverse().join("\n\n"+L+"\n\n")+"\n\n"+L
+
+}
+
+replier.reply("[!] '"+send+"' 님의 채팅 기록입니다."+Lw+"\n\n총 "+sendchat.length+"개\n\n"+rep)
+
+} else {
+
+replier.reply("[!] 해당 유저의 채팅 기록이 존재하지 않습니다!")
+
+}
+
+}
+
+//채팅 기록 (방)
+
+if (msg.startsWith("!채팅기록")) {
+
+ep = msg.substr(5).trim()
+
+if (ep == "") {
+
+if (!back) {
+
+rep = L+"\n\n"+JSON.parse(FS.read(chatpath))[room].join("\n\n"+L+"\n\n")+"\n\n"+L
+
+} else {
+
+rep = L+"\n\n"+JSON.parse(FS.read(chatpath))[room].reverse().join("\n\n"+L+"\n\n")+"\n\n"+L
+
+}
+
+replier.reply("[!] '"+room+"' 의 채팅 기록입니다."+Lw+"\n\n총 "+(JSON.parse(FS.read(chatpath))[room]).length+"개\n\n"+rep)
+
+} else if (ep == "전체") {
+
+if (!back) {
+
+rep = L+"\n\n"+JSON.parse(FS.read(chatpath))[all].join("\n\n"+L+"\n\n")+"\n\n"+L
+
+} else {
+
+rep = L+"\n\n"+JSON.parse(FS.read(chatpath))[all].reverse().join("\n\n"+L+"\n\n")+"\n\n"+L
+
+}
+
+replier.reply("[!] 종합 채팅 기록입니다."+Lw+"\n\n총 "+(JSON.parse(FS.read(chatpath))[all]).length+"개\n\n"+rep)
+
+} else {
+
+if (JSON.parse(FS.read(chatpath))[ep]) {
+
+if (!back) {
+
+rep = L+"\n\n"+JSON.parse(FS.read(chatpath))[ep].join("\n\n"+L+"\n\n")+"\n\n"+L
+
+} else {
+
+rep = L+"\n\n"+JSON.parse(FS.read(chatpath))[ep].reverse().join("\n\n"+L+"\n\n")+"\n\n"+L
+
+}
+
+replier.reply("[!] '"+ep+"' 의 채팅 기록입니다."+Lw+"\n\n총 "+(JSON.parse(FS.read(chatpath))[ep]).length+"개\n\n"+rep)
+
+} else {
+
+replier.reply("[!] 해당 방의 채팅 기록이 존재하지 않습니다!")
+
+}
+
+}
+
+}
+
+if (msg == "!방목록") {
+
+json = JSON.parse(FS.read(chatpath))
+
+roomlist = Object.keys(json)
+
+roomlist.splice(roomlist.indexOf(all), 1)
+
+roomlist = roomlist.map(a=>"이름 : "+a+"\n총 채팅 수 : "+json[a].length+"회")
+
+replier.reply("[!] 채팅 기록이 저장되어 있는 방 목록입니다."+Lw+"\n\n총 "+roomlist.length+"개\n\n"+roomlist.join("\n\n"))
+
+}
+
+if (admins.indexOf(sender)!=-1) {
+
+if (msg.startsWith("!채팅초기화")) {
+
+ep = msg.substr(6).trim()
+
+if (ep == "") {
+
+json = JSON.parse(FS.read(chatpath))
+
+delete json[room]
+
+FS.write(chatpath, JSON.stringify(json))
+
+replier.reply("[!] '"+room+"' 방의 채팅 기록이 초기화 되었습니다!")
+
+} else if (ep == "전체") {
+
+FS.write(chatpath, "{}")
+
+replier.reply("[!] 모든 채팅 기록이 초기화 되었습니다!")
+
+} else {
+
+json = JSON.parse(FS.read(chatpath))
+
+delete json[ep]
+
+FS.write(chatpath, JSON.stringify(json))
+
+replier.reply("[!] '"+ep+"' 방의 채팅 기록이 초기화 되었습니다!")
+
+}
+
+}
+
+}
+
+} catch(e) {
+
+FS.write(chatpath, "{}")
+
+}
 
 
     if (arr_room.indexOf(room) == -1) {
