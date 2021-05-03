828f82f1a8ca484c84fef80806484f7ba752cddf
answhdgkr95 <49180647+answhdgkr95@users.noreply.github.com>
2021-05-03 21:59:33 +0900

    babbot spring 추가 1.0.0.0
    문종학 작업

---
+++ babbot/coin/coin/src/main/java/com/babbot/coin/CoinApplication.java
@@ -0,0 +1,13 @@
+package com.babbot.coin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinApplication.class, args);
+	}
+
+}
---
+++ babbot/coin/coin/src/test/java/com/babbot/coin/CoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babbot.coin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
